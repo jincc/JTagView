8dfc69771767850bd8a4de2177e127dbf72d2f42
jiangjunlong <junlongj@jumei.com>
2016-02-16 11:44:12 +0800

    Initial Commit

---
+++ JTagView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTagView
+//
+//  Created by junlongj on 16/2/16.
+//  Copyright © 2016年 junlongj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTagView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JTagView
+//
+//  Created by junlongj on 16/2/16.
+//  Copyright © 2016年 junlongj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
